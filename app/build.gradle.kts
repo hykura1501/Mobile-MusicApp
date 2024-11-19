9339a860bb3179ff3d4406fa432b23c6ec9338df
Do Thai Hoc <78217280+hocvn@users.noreply.github.com>
2024-11-19 16:04:43 +0700

    #1 - create basic music player fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mobile_musicapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mobile_musicapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
