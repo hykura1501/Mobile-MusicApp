05217b7f51cf6cf44633794c4c6b991928b45eee
22120106 <22120106@student.hcmus.edu.vn>
2024-12-12 11:23:53 +0700

    #2 - Finish form login and register

--- app/src/main/java/com/example/mobile_musicapp/MainActivity.kt
+++ app/src/main/java/com/example/mobile_musicapp/MainActivity.kt
@@ -5,23 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mobile_musicapp.Authentication.Register
 import com.example.mobile_musicapp.playMusicFragment.PlayMusic
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_auth)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-//        if (savedInstanceState == null) {
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.fragment_container, PlayMusic())
-//                .commit()
-//        }
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, Register())
+                .commit()
+        }
     }
 }
