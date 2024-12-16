92bbb8860cc2a06e89428ead24ed52cfe9bbba73
22120106 <22120106@student.hcmus.edu.vn>
2024-12-16 16:25:31 +0700

    call api login and register

--- app/src/main/java/com/example/mobile_musicapp/services/AuthDao.kt
+++ app/src/main/java/com/example/mobile_musicapp/services/AuthDao.kt
@@ -1,28 +1,46 @@
 package com.example.mobile_musicapp.services
 
+import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+
+
+sealed class AuthApiResult {
+    data class Success(val data: ApiResponseAuth?) : AuthApiResult()
+    data class Error(val code: Int, val message: String) : AuthApiResult()
+}
+
 class AuthDao {
     companion object {
-        suspend fun login(username: String, password: String): String? {
+        suspend fun login(username: String, password: String): AuthApiResult? {
             return try {
                 val loginRequest = LoginRequest(username, password)
                 val response = RetrofitClient.instance.login(loginRequest)
                 if (response.isSuccessful) {
-                    response.body()?.token
+                    AuthApiResult.Success(response.body())
                 } else {
-                    null
+                    val errorBody = response.errorBody()?.string()
+                    val errorJson = Gson().fromJson(errorBody, JsonObject::class.java)
+                    val errorMessage = errorJson["message"]?.asString ?: "Unknown error"
+                    val statusCode = errorJson["code"]?.asInt ?: -1
+                    AuthApiResult.Error(statusCode, errorMessage)
                 }
             } catch (e: Exception) {
                 null
             }
         }
-        suspend fun register(fullName: String, email: String, password: String): String? {
+        suspend fun register(fullName: String, email: String, password: String): AuthApiResult? {
             return try {
                 val registerRequest = RegisterRequest(fullName, email, password)
                 val response = RetrofitClient.instance.register(registerRequest)
                 if (response.isSuccessful) {
-                    response.body()?.token
+                    AuthApiResult.Success(response.body())
                 } else {
-                    null
+                    val errorBody = response.errorBody()?.string()
+                    val errorJson = Gson().fromJson(errorBody, JsonObject::class.java)
+                    val errorMessage = errorJson["message"]?.asString ?: "Unknown error"
+                    val statusCode = errorJson["code"]?.asInt ?: -1
+                    AuthApiResult.Error(statusCode, errorMessage)
                 }
             } catch (e: Exception) {
                 null
